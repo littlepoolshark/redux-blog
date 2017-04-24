cee0ff1f61fdc9e826b9c5af0f3f376edfc05804
sam liu <1145694956@qq.com>
2017-04-24 16:46:28 +0800

    几乎完成了无缝连接并且支持拖拽的轮播图，但是还有动画，交互效果还有很大的进步空间

--- src/components/UI/Slider/Slider.js
+++ src/components/UI/Slider/Slider.js
@@ -1,4 +1,4 @@
-//@flow
+
 import React, { Component, PropTypes } from 'react';
 import Hammer from 'hammerjs';
 import './Slider.scss';
@@ -33,21 +33,20 @@ class Slider extends Component {
     }
 
     _handleTransitionEnd() {
-        let {
-         index,
-         direction
-     } = this.state;
+        console.log("into _handleTransitionEnd")
+        let className=this.refs.sliderWrapper.className;
+        this.refs.sliderWrapper.className = className.replace('animate', '').trim();
 
-        if (direction === "prev" && index === 0) {
-            this.setState({
-                index: 4
-            })
+
+        if (this.currIndex === 0) {
+            this.currIndex=4;
+            this.refs.sliderWrapper.style.transform=`translate3d(${ -SCREENWIDTH * 4 }px,0,0)`;
         }
-        if (direction === "next" && index === 5) {
-            this.setState({
-                index: 1
-            })
+        if (this.currIndex === 5) {
+            this.currIndex=1;
+           this.refs.sliderWrapper.style.transform=`translate3d(${ -SCREENWIDTH * 1 }px,0,0)`;
         }
+       
     }
 
     _enableScroll() {
@@ -64,6 +63,14 @@ class Slider extends Component {
         }
     }
 
+     getCurrentTranslateX() {
+        let transfromStr = this.refs.sliderWrapper.style.transform;
+        let indexOfLeftBrace = transfromStr.indexOf("(");
+        let prevTranslateX = parseInt(transfromStr.slice(indexOfLeftBrace + 1, -1).split(",")[0]);
+
+        return prevTranslateX;
+    }
+
     _handlePan(e){ 
     //    console.log('e',e);
     //    let transfromStr= this.refs.sliderWrapper.style.transform;
@@ -76,7 +83,6 @@ class Slider extends Component {
     //         nextTranslateX=prevTranslateX + 2;
     //    }
       let nextTranslateX=this.currTranslateX + e.deltaX;
-
        this.refs.sliderWrapper.style.transform=`translate3d(${ nextTranslateX }px,0,0)`
     }
 
@@ -109,7 +115,7 @@ class Slider extends Component {
                     className="slider-wrapper"
                     ref="sliderWrapper"
                     style={sliderWrapperStyle}
-                    onTransitionEnd={(e) => { /*this._handleTransitionEnd()*/ }}
+                    onTransitionEnd={(e) => { this._handleTransitionEnd() }}
                 >
                     <li>4</li>
                     <li>1</li>
@@ -131,139 +137,46 @@ class Slider extends Component {
             element.style.width = SCREENWIDTH + "px";
         }
 
+        this.currIndex=1;
         this.hammertime=new Hammer(this.refs.sliderWrapper);
-        //this.hammertime.get('swipe').set({ velocity: 0.1 ,direction:Hammer.DIRECTION_HORIZONTAL});        
-        //this.hammertime.get('pan').set({ threshold: 50 ,direction:Hammer.DIRECTION_HORIZONTAL});
-        this.hammertime.get('pan').dropRecognizeWith(this.hammertime.get('swipe'));
+        this.hammertime.get('swipe').set({ velocity: 0.1 ,direction:Hammer.DIRECTION_HORIZONTAL});        
+        //this.hammertime.get('pan').set({ threshold: 10 ,direction:Hammer.DIRECTION_HORIZONTAL});
+        //this.hammertime.get('pan').dropRecognizeWith(this.hammertime.get('swipe'));
 
         this.hammertime.on("panstart",(e) => {
-            let transfromStr= this.refs.sliderWrapper.style.transform;
-            let indexOfLeftBrace= transfromStr.indexOf("(");
-            let prevTranslateX=parseInt(transfromStr.slice(indexOfLeftBrace + 1,-1).split(",")[0]);
+            //debug.log('into panstart');
+            let prevTranslateX=this.getCurrentTranslateX();
             this.currTranslateX=prevTranslateX + e.deltaX;
         });
         this.hammertime.on("panmove",(e) => {
-            //alert('panmove');
+            //debug.log('into panmove');
             this._handlePan(e)
         });
-        // this.hammertime.on("swipeleft",(e) => {
-        //     alert('into swipeleft:');
-        // });
-        // this.hammertime.on("swiperight",(e) => {
-        //     alert('into swiperight:');
-        // });
-    }
 
-    some(){
-
-
-        var reqAnimationFrame = (function() {
-            return window[Hammer.prefixed(window, "requestAnimationFrame")] || function(callback) {
-                setTimeout(callback, 1000 / 60);
-            }
-        })();
-
-        function dirProp(direction, hProp, vProp) {
-            return (direction & Hammer.DIRECTION_HORIZONTAL) ? hProp : vProp
-        }
-
-
-        /**
-         * Carousel
-         * @param container
-         * @param direction
-         * @constructor
-         */
-        function HammerCarousel(container, direction) {
-            this.container = container;
-            this.direction = direction;
-
-            this.panes = Array.prototype.slice.call(this.container.children, 0);
-            this.containerSize = this.container[dirProp(direction, 'offsetWidth', 'offsetHeight')];
-
-            this.currentIndex = 0;
-
-            this.hammer = new Hammer.Manager(this.container);
-            this.hammer.add(new Hammer.Pan({ direction: this.direction, threshold: 10 }));
-            this.hammer.on("panstart panmove panend pancancel", Hammer.bindFn(this.onPan, this));
-
-            this.show(this.currentIndex);
-        }
-
-
-        HammerCarousel.prototype = {
-            /**
-             * show a pane
-             * @param {Number} showIndex
-             * @param {Number} [percent] percentage visible
-             * @param {Boolean} [animate]
-             */
-            show: function(showIndex, percent, animate){
-                showIndex = Math.max(0, Math.min(showIndex, this.panes.length - 1));
-                percent = percent || 0;
-
-                var className = this.container.className;
-                if(animate) {
-                    if(className.indexOf('animate') === -1) {
-                        this.container.className += ' animate';
-                    }
-                } else {
-                    if(className.indexOf('animate') !== -1) {
-                        this.container.className = className.replace('animate', '').trim();
-                    }
+        this.hammertime.on("panend pancancel",(e) => {
+            //debug.log('into panend');
+           let precent=(Math.abs(this.getCurrentTranslateX() - this.currTranslateX ) + 10 )* 100  / SCREENWIDTH;
+           if(e.deltaX < 0){
+                if(precent > 20){
+                    this.currIndex +=1;
                 }
-
-                var paneIndex, pos, translate;
-                for (paneIndex = 0; paneIndex < this.panes.length; paneIndex++) {
-                    pos = (this.containerSize / 100) * (((paneIndex - showIndex) * 100) + percent);
-                    if(this.direction & Hammer.DIRECTION_HORIZONTAL) {
-                        translate = 'translate3d(' + pos + 'px, 0, 0)';
-                    } else {
-                        translate = 'translate3d(0, ' + pos + 'px, 0)'
-                    }
-                     this.panes[paneIndex].style.transform = translate;
-                     this.panes[paneIndex].style.mozTransform = translate;
-                     this.panes[paneIndex].style.webkitTransform = translate;
+           }else {
+                 if(precent > 20){
+                    this.currIndex -=1;
                 }
+           }
+           let nextTranslateX=-SCREENWIDTH * this.currIndex ;
+           this.refs.sliderWrapper.className += " animate";
+           this.refs.sliderWrapper.style.transform=`translate3d(${ nextTranslateX }px,0,0)`;
 
-                this.currentIndex = showIndex;
-            },
-
-            /**
-             * handle pan
-             * @param {Object} ev
-             */
-            onPan : function (ev) {
-                var delta = dirProp(this.direction, ev.deltaX, ev.deltaY);
-                var percent = (100 / this.containerSize) * delta;
-                var animate = false;
-
-                if (ev.type == 'panend' || ev.type == 'pancancel') {
-                    if (Math.abs(percent) > 20 && ev.type == 'panend') {
-                        this.currentIndex += (percent < 0) ? 1 : -1;
-                    }
-                    percent = 0;
-                    animate = true;
-                }
-
-                this.show(this.currentIndex, percent, animate);
-            }
-        };
-
-        // the horizontal pane scroller
-        var outer = new HammerCarousel(document.querySelector(".panes.wrapper"), Hammer.DIRECTION_HORIZONTAL);
-
-        // each pane should contain a vertical pane scroller
-        Hammer.each(document.querySelectorAll(".pane .panes"), function(container) {
-            // setup the inner scroller
-            var inner = new HammerCarousel(container, Hammer.DIRECTION_VERTICAL);
-
-            // only recognize the inner pan when the outer is failing.
-            // they both have a threshold of some px
-            outer.hammer.get('pan').requireFailure(inner.hammer.get('pan'));
         });
 
-    
+        // this.hammertime.on("swipeleft",(e) => {
+        //     //debug.log('into swipeleft:');
+        // });
+        // this.hammertime.on("swiperight",(e) => {
+        //     //debug.log('into swiperight:');
+        // });
     }
 
     componentDidUpdate() {
